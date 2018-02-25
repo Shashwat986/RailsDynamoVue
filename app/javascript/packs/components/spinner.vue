694664a2c23e427729eb3ebce5915e869775ee58
Shashwat Chandra <chandra.shashwat@gmail.com>
2018-02-25 21:28:26 +0530

    Good loader

--- app/javascript/packs/components/spinner.vue
+++ app/javascript/packs/components/spinner.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="fixed-top container">
     <div class="row justify-content-center">
-      <span class="alert alert-success notice-alert text-center mt-1">Loading...</span>
+      <div class="progress col-2 p-0 mt-2" style="height: 3em;">
+        <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" style="width: 100%">Loading...</div>
+      </div>
     </div>
   </div>
 </template>
